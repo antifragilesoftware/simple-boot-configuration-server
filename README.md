5c0c1549dc18197f338a123e82e63572400f5436
Russ Miles <russ@russmiles.com>
2016-04-05 09:49:39 -0400

    Added first version of config server

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# simple-boot-configuration-server
+# Sample Spring Boot Configuration Service
+
+## To Build into Docker using Maven
+
+First make sure you're running in an environment that has docker available to you. Then execute:
+
+$ mvn package docker:build
+
+Once completed you will have an antifragilesoftware/simple-boot-configuration-server image available, as seen by executing:
+
+$ docker images
+
+```
+REPOSITORY                         TAG                 IMAGE ID            CREATED             SIZE
+antifragilesoftware/simple-boot-configuration-server   latest              6efd07dc12fc        5 seconds ago       667.1 MB
+```
---
+++ src/main/java/com/russmiles/antifragilesoftware/configurationserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.russmiles.antifragilesoftware.configurationserver;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ConfigServerApplication.class).run(args);
+	}
+}
